97123e9f5a9b3ec5f70919644c58b4429dfd58dd
lokesh198maurya <lokesh95.maurya@gmail.com>
2021-03-24 23:57:46 +0530

    Added searching to using user profile

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profile_api import serializers
-
+from profile_api import models
+from profile_api import permissions
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializers
@@ -47,3 +50,57 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Handle delte request"""
         return Response({'Msg': "Delete request method"})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializers
+
+    def list(self, request):
+        """Return a hello msg"""
+        a_viewset = [
+            'Uses action (list, create, retrive, update, partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'msg': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello msg"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            msg = f'Hello {name}!'
+            return Response({'msg': msg})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrive(self, request, pk=None):
+        """Handle object by its id"""
+        return Response({'http_method': 'GET'})
+    
+    def update(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Handle updating part of an object"""
+        return Response({'http_method': 'PTACH'})
+
+    def destroy(self, request, pk=None):
+        """Handle removing an object"""
+        return Response({'http_method': 'Delete'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
